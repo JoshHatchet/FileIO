ef60d0b8f7befbb1404e626b77c6b183a75a63fd
JoshHatchet <joshbordick@gmail.com>
2021-02-17 15:24:22 -0700

    added new files and fixed code

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -5,14 +5,27 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class ScoreTrakker {
+	private static String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 	private ArrayList<Student> students = new ArrayList<Student>();
 	public void loadDataFromFile(String fileName) throws FileNotFoundException {
+		students.clear();
 		FileReader reader = new FileReader(fileName);
 		Scanner readFile = new Scanner(reader);
+		
 		while(readFile.hasNextLine()) { 
 			String name = readFile.nextLine();
-			int score = Integer.parseInt(readFile.nextLine());
-			students.add(new Student(name, score));
+			String line = readFile.nextLine();
+			try {
+				
+				int score = Integer.parseInt(line);
+				students.add(new Student(name, score));
+			}
+			catch(NumberFormatException e) {
+				System.out.println();
+				System.out.println("Incorect Formatting for " + name + " not a valid score: " + line);
+				System.out.println();
+			}
+
 		}
 	}
 	public void printInOrder() {
@@ -21,17 +34,27 @@ public class ScoreTrakker {
 			System.out.println(i.toString());
 		}
 	}
-	public void processFiles(String inFile) throws FileNotFoundException {
-		loadDataFromFile(inFile);
-		printInOrder();
+	public void processFiles(String[] files)  {
+		for(String i: files) {
+			try {
+				loadDataFromFile(i);
+				printInOrder();
+			}
+			catch(FileNotFoundException e) {
+				System.out.println();
+				System.out.println("Cannot Open File: " + i);
+				System.out.println();
+			}
+		
+		}
+		
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please Enter a File Name: ");
-		String inputFile = scanner.next();
+	public static void main(String[] args) {
 		ScoreTrakker test = new ScoreTrakker();
-		test.processFiles(inputFile);
+		test.processFiles(files);
+
+	
 	}
 
 }
